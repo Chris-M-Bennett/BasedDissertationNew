851b348c31bed876e57c9218178ffc7397ffabf5
Chris-M-Bennett <cmbennett1@hotmail.com>
2022-05-17 00:22:05 +0100

    Initial

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using EventSystem;
+using UnityEngine.Events;
+
+public class IntUnityEvent : UnityEvent<int>{
+}
+public class EventManager : GenericSingleton<EventManager>
+{
+     private Dictionary<string, IntUnityEvent> _eventDictionary;
+            public IntUnityEvent intEvent;
+    
+            public override void Awake(){
+                base.Awake();
+                if(Instance._eventDictionary == null){
+                    Instance._eventDictionary = new Dictionary<string, IntUnityEvent>();
+                }
+                if(Instance.intEvent == null){
+                    Instance.intEvent = new IntUnityEvent();
+                }
+            }
+    
+            public void StartListening(string eventName, UnityAction<int> listener){
+                IntUnityEvent thisEvent;
+                if (Instance._eventDictionary.TryGetValue(eventName, out thisEvent)){
+                    //Event name is in dictionary
+                    thisEvent.AddListener(listener);
+                }else{
+                    thisEvent = new IntUnityEvent();
+                    thisEvent.AddListener(listener);
+                    Instance._eventDictionary.Add(eventName, thisEvent);
+                } 
+            }
+            public void StopListening(string eventName, UnityAction<int> listener){
+                if (Instance._eventDictionary == null){
+                    return;
+                }
+                IntUnityEvent thisEvent;
+                if (Instance._eventDictionary.TryGetValue(eventName, out thisEvent)){
+                    thisEvent.RemoveListener(listener);
+                }
+            }
+            public void TriggerEvent(string eventName){
+                IntUnityEvent thisEvent = null;
+                if (Instance._eventDictionary.TryGetValue(eventName, out thisEvent)){
+                    thisEvent.Invoke(0);
+                } 
+            }
+}
---
+++ Assets/Scripts/HandPresence.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    [SerializeField] private InputDeviceCharacteristics controllerChars;
+    [SerializeField] private GameObject handPrefab;
+    private InputDevice _targetDevice;
+    private GameObject _spawnedHand;
+    private Animator handAnim;
+    private static readonly int Trigger = Animator.StringToHash("Trigger");
+    private static readonly int Grip = Animator.StringToHash("Grip");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerChars, devices);
+
+        if (devices.Count > 0)
+        {
+            _targetDevice = devices[0];
+            _spawnedHand = Instantiate(handPrefab,transform);
+            handAnim = _spawnedHand.GetComponent<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnim.SetFloat(Trigger, triggerValue);
+        }else
+        {
+            handAnim.SetFloat(Trigger, 0);  
+        }
+        
+        if (_targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnim.SetFloat(Grip, gripValue);
+        }else
+        {
+            handAnim.SetFloat(Grip, 0);  
+        }
+    }
+}
